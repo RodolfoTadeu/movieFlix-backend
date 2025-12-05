1fda16fbe42253edb337e241df643dd0c2aa6923
rodolfotadeu <rodolfodevdesigner@gmail.com>
2025-12-04 23:41:31 -0300

    chore: add migrations

---
+++ prisma/migrations/20251201180411_add_person/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Person" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "image" TEXT,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
